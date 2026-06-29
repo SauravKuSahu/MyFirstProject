cfb6d761dfe83f62c668aededa7fc727a043aa56
SauravKuSahu <callsauravsahu@gmail.com>
2026-06-30 03:12:19 +0530

    adding code from new branch

---
+++ src/collection/PaymentPage.java
@@ -0,0 +1,12 @@
+
+package collection;
+
+public class PaymentPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am in Code-Snippets or Different branch0");
+
+	}
+
+}
